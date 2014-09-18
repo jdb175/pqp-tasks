ca32808c62b9a5e50e404d72002070db967c9962
Jason Whitehouse <jdb175@gmail.com>
2014-09-18 09:06:05 -0400

    Initial Commit

---
+++ PQPTasks/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  PQPTasks
+//
+//  Created by Jason Whitehouse on 9/18/14.
+//  Copyright (c) 2014 WPI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
