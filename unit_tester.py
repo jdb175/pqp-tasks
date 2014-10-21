16bf20bd25ac3fd764ef347a861ba8cc3dc9eeb4
Jason Whitehouse <jawhit@Jasons-Mac-mini-2.local>
2014-10-21 16:23:05 -0400

    added unit_tester.py

---
+++ unit_tester.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+import readline
+
+def run_unit_tests(scheme, device_name, configuration):
+
+	print "\nRunning Unit Tests ..."
+
+	unit_test = "xcodebuild test -scheme " + scheme + " -destination name='" + device_name + "' -configuration " + configuration 
+
+	try:
+		unit_test_res = subprocess.check_output(unit_test, shell=True)
+	
+		test_start_index = unit_test_res.find("Test Suite")
+
+		print unit_test_res[test_start_index:]
+
+	except subprocess.CalledProcessError as error:
+		print "Error: " + error.cmd 
+		sys.exit(1)	
+
+def main():
+	scheme = raw_input("Please enter a scheme: ")
+
+	device_name = raw_input("Please enter a device name: ")
+
+	configuration = raw_input("Please enter a configuration: ")
+
+	run_unit_tests(scheme, device_name, configuration)
+
+
+if __name__ == "__main__":
+	main()
