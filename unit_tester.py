69f3b15eef91955e0401c22dfa7367d23fe484a7
Jason Whitehouse <jawhit@Jasons-Mac-mini-2.local>
2014-10-21 16:31:45 -0400

    Changed sys.exit to 2 to create conflict

--- unit_tester.py
+++ unit_tester.py
@@ -9,15 +9,19 @@ def run_unit_tests(scheme, device_name, configuration):
 	unit_test = "xcodebuild test -scheme " + scheme + " -destination name='" + device_name + "' -configuration " + configuration 
 
 	try:
-		unit_test_res = subprocess.check_output(unit_test, shell=True)
+		unit_test_res = subprocess.check_output(unit_test, shell=True, stderr=subprocess.STDOUT,)
 	
-		test_start_index = unit_test_res.find("Test Suite")
+		#test_start_index = unit_test_res.find("Test Suite")
 
-		print unit_test_res[test_start_index:]
+		#print unit_test_res[test_start_index:]
+
+		return 1
 
 	except subprocess.CalledProcessError as error:
 		print "Error: " + error.cmd 
-		sys.exit(1)	
+		sys.exit(2)	
+		return 0
+
 
 def main():
 	scheme = raw_input("Please enter a scheme: ")
